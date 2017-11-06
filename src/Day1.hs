6a03d71d7a6e7d0c6720bc25c38823929b351952
Jacob Lorenzen <lorenzen.jacob@gmail.com>
2017-11-06 19:41:28 +0100

    compose functions

--- src/Day1.hs
+++ src/Day1.hs
@@ -4,17 +4,33 @@ module Day1 (parseInput, move, moves, Move(R, L)) where
   import qualified Data.Text as T
 
   data Move = R Int | L Int deriving (Eq, Show)
+  data Position = Position Int Int
+  data Coord = Coord Position Move
 
-  parseInput :: String -> [String]
-  parseInput = map trim . splitOn ","
+  result :: String -> Int
+  result = blocks . calculate . moves
+
+  blocks :: Coord -> Int
+  blocks (Coord (Position x y) _) =
+    x + y
   
+  calculate :: [Move] -> Coord
+  calculate (x:xs) = foldl acc (Coord (Position 0 0) x) xs
+
+  acc :: Coord -> Move -> Coord
+  acc coord move =
+    Coord (Position 0 0) move
+
   moves :: String -> [Move]
   moves = map move . parseInput
 
   move :: String -> Move
-  move m@(x:xs) 
+  move (x:xs) 
     | x == 'R' = R (read xs)
     | otherwise = L (read xs)
-    
+
+  parseInput :: String -> [String]
+  parseInput = map trim . splitOn ","
+  
   trim :: String -> String
   trim = T.unpack . T.strip . T.pack 
