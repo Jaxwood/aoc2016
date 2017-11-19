cdda5ed20d8e3fddd4ece9aabd7bd08b21d6d963
Jacob Lorenzen <lorenzen.jacob@gmail.com>
2017-11-19 07:48:42 +0100

    failing test for day5a

--- src/Day5.hs
+++ src/Day5.hs
@@ -1,7 +1,7 @@
 module Day5 (resulta, resultb) where
 
-  resulta :: String -> Int
-  resulta str = 0
+  resulta :: String -> String
+  resulta str = ""
 
   resultb :: String -> Int
   resultb str = 0
