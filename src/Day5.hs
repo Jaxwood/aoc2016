33292063e65b0d2d1e689ac710699db2c560bbf6
Jacob Lorenzen <lorenzen.jacob@gmail.com>
2017-11-19 08:31:11 +0100

    add cryptohash and bytestring modules

--- src/Day5.hs
+++ src/Day5.hs
@@ -1,7 +1,14 @@
 module Day5 (resulta, resultb) where
 
+  import qualified Data.ByteString.Lazy as LB
+  import qualified Data.ByteString.Builder as LBB
+  import Crypto.Hash
+
   resulta :: String -> String
-  resulta str = ""
+  resulta = show . md5 . LBB.toLazyByteString . LBB.stringUtf8
 
   resultb :: String -> Int
   resultb str = 0
+
+  md5 :: LB.ByteString -> Digest MD5
+  md5 = hashlazy
