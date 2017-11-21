7fc3ff507c2cf6e1d26fd279cc5191aee06947b9
Jacob Lorenzen <lorenzen.jacob@gmail.com>
2017-11-21 21:40:17 +0100

    find palindromes

--- src/Day7.hs
+++ src/Day7.hs
@@ -1,10 +1,22 @@
-module Day7 (resulta, resultb) where
+module Day7 (resulta, resultb, palindrome) where
 
   import Data.List
   import Data.Function
+
+  data IPv7 = IPv7
 
   resulta :: String -> String
   resulta = id
 
   resultb :: String -> String
   resultb = id
+
+  palindrome :: String -> Bool
+  palindrome s@(a:b:c:d:xs)
+    | a == d && b == c && a /= b && c /= d = True
+    | otherwise = palindrome $ tail s
+  palindrome _ =
+    False
+
+  parse :: String -> IPv7
+  parse s = IPv7
--- tests/Day7Tests.hs
+++ tests/Day7Tests.hs
@@ -1,9 +1,12 @@
 module Day7Tests where
   import Test.HUnit
-  import Day7 (resulta, resultb)
+  import Day7 (resulta, resultb, palindrome)
 
   day7 :: String -> Test
   day7 str = test [
       assertEqual "day7a" "" (resulta str),
-      assertEqual "day7b" "" (resultb str)
+      assertEqual "day7b" "" (resultb str),
+      assertEqual "palindrome" True (palindrome "ABBA"),
+      assertEqual "palindrome" False (palindrome "aaaa"),
+      assertEqual "palindrome" True (palindrome "ioxxoj")
     ]
