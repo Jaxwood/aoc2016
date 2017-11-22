92c66cb536f820904ba1c7b557c6652ecca49c7b
Jacob Lorenzen <lorenzen.jacob@gmail.com>
2017-11-22 19:59:44 +0100

    support for many hypernet and abba

--- src/Day7.hs
+++ src/Day7.hs
@@ -1,4 +1,4 @@
-module Day7 (resulta, resultb, palindrome, toIPv7, IPv7(IPv7), verify) where
+module Day7 (resulta, resultb) where
 
   import Data.List
   import Data.Function
@@ -23,17 +23,16 @@ module Day7 (resulta, resultb, palindrome, toIPv7, IPv7(IPv7), verify) where
     False
 
   verify :: Either ParseError IPv7 -> Bool
-  verify (Right (IPv7 a b)) = all id [all palindrome a, all (not . palindrome) b]
+  verify (Right (IPv7 a b)) = all id [any palindrome a, all (not . palindrome) b]
   verify (Left _) = error "parse error"
 
   toIPv7 :: String -> Either ParseError IPv7
   toIPv7 = parse match ""
 
+  sub :: (Int -> Bool) -> [String] -> [String]
+  sub fn xs = map fst $ filter (fn . snd) $ zip xs [1..]
+
   match :: Parser IPv7
   match = do
-    ab <- many1 letter
-    _ <- char '['
-    hypernet <- many1 letter
-    _ <- char ']'
-    ba <- many1 letter
-    return $ IPv7 [] []
+    ab <- sepBy (many letter) $ oneOf ['[', ']']
+    return $ IPv7 (sub odd ab) (sub even ab) 
