d58694286bad5278a617530cbd079af42eeb8f5c
Jacob Lorenzen <lorenzen.jacob@gmail.com>
2017-11-24 12:56:47 +0100

    follow instruction

--- src/Day8.hs
+++ src/Day8.hs
@@ -1,4 +1,4 @@
-module Day8 (resulta, resultb, rotate, toInstructions, Instruction(On,Row,Column)) where
+module Day8 (resulta, resultb, rotate, toInstructions, Instruction(On,Row,Column), row, update) where
 
   import Data.Either (rights)
   import Data.List
@@ -7,7 +7,9 @@ module Day8 (resulta, resultb, rotate, toInstructions, Instruction(On,Row,Column
 
   data Instruction = On Int Int | Row Int Int | Column Int Int deriving (Eq, Show)
 
-  rect :: [[Int]]
+  type Screen = [[Int]]
+
+  rect :: Screen
   rect = concat [take 6 $ repeat x | x <- [take 50 $ repeat 0]]
 
   resulta :: String -> Int
@@ -27,6 +29,24 @@ module Day8 (resulta, resultb, rotate, toInstructions, Instruction(On,Row,Column
   rotate' xs i =
     let (a, b) = splitAt i xs
     in b ++ a
+
+  update :: [[Int]] -> Instruction -> [[Int]]
+  update ss i@(On x y) =
+    map ((\(a, b) -> if b < y then row i a else a)) $ map id $ zip ss [0..]
+
+  update ss i@(Row x y) =
+    map ((\(a, b) -> if b == x then rotate a y else a)) $ map id $ zip ss [0..]
+
+  update ss i@(Column x y) =
+    transpose $ map ((\(a, b) -> if a == x then rotate b y else b)) $ map id $ zip [0..] $ transpose ss
+
+  row :: Instruction -> [Int] -> [Int]
+  row (On x y) s =
+    map (\(a, b) -> if b < x then 1 else a) $ zip s [0..]
+    
+  row (Column x y) s = s
+
+  row (Row x y) s = s
 
   toInstructions :: String -> [Instruction]
   toInstructions = rights . map (parse (choice [try matchOn, try matchRow, matchColumn]) "") . lines
--- tests/Day8Tests.hs
+++ tests/Day8Tests.hs
@@ -1,6 +1,6 @@
 module Day8Tests where
   import Test.HUnit
-  import Day8 (resulta, resultb, rotate, toInstructions, Instruction(On,Row,Column))
+  import Day8 (resulta, resultb, rotate, toInstructions, Instruction(On,Row,Column), row, update)
 
   day8 :: String -> Test
   day8 a = test [
@@ -13,5 +13,11 @@ module Day8Tests where
       assertEqual "rotate" [0,1,0] (rotate [0,0,1] 5),
       assertEqual "rotate" [0,0,1] (rotate [0,0,1] 6),
       assertEqual "rotate" [1,0,0] (rotate [0,0,1] 7),
-      assertEqual "instructions" [] (toInstructions a)
+      assertEqual "instructions" [On 3 2, Column 1 1, Row 0 4, Column 1 1] (toInstructions a),
+      assertEqual "row" [1,0,0] (row (On 1 1) [0,0,0]),
+      assertEqual "row" [1,1,1] (row (On 1 1) [0,1,1]),
+      assertEqual "set update" [[1,1,1,0,0,0], [1,1,1,0,0,0], [0,0,0,0,0,0]] (update [[0,0,0,0,0,0], [0,0,0,0,0,0], [0,0,0,0,0,0]] (On 3 2)),
+      assertEqual "rotate row" [[0,0,0,1,0,1], [0,0,0,0,0,0], [0,0,0,0,0,0]] (update [[1,0,1,0,0,0], [0,0,0,0,0,0], [0,0,0,0,0,0]] (Row 0 3)),
+      assertEqual "rotate row" [[1,0,1,0,0,0], [0,0,0,0,0,1], [0,0,0,0,0,0]] (update [[1,0,1,0,0,0], [0,0,0,1,0,0], [0,0,0,0,0,0]] (Row 1 2)),
+      assertEqual "rotate column" [[0,0,0,0,0,0], [1,0,0,0,0,0], [0,0,0,0,0,0]] (update [[1,0,0,0,0,0], [0,0,0,0,0,0], [0,0,0,0,0,0]] (Column 0 1))
     ]
