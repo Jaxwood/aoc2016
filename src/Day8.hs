657139adbb9b567dfcfd9a65b3ebb44e97f87595
Jacob Lorenzen <lorenzen.jacob@gmail.com>
2017-11-24 11:05:40 +0100

    parse input

--- src/Day8.hs
+++ src/Day8.hs
@@ -1,8 +1,11 @@
-module Day8 (resulta, resultb, rotate) where
+module Day8 (resulta, resultb, rotate, toInstructions, Instruction(On,Row,Column)) where
 
+  import Data.Either (rights)
   import Data.List
   import Text.Parsec
   import Text.Parsec.String
+
+  data Instruction = On Int Int | Row Int Int | Column Int Int deriving (Eq, Show)
 
   rect :: [[Int]]
   rect = concat [take 6 $ repeat x | x <- [take 50 $ repeat 0]]
@@ -24,4 +27,44 @@ module Day8 (resulta, resultb, rotate) where
   rotate' xs i =
     let (a, b) = splitAt i xs
     in b ++ a
+
+  toInstructions :: String -> [Instruction]
+  toInstructions = rights . map (parse (choice [try matchOn, try matchRow, matchColumn]) "") . lines
+
+  matchOn :: Parser Instruction
+  matchOn = do
+    _ <- string "rect"
+    _ <- space
+    a <- many digit
+    _ <- char 'x'
+    b <- many digit
+    return $ On (read a) (read b)
+
+  matchRow :: Parser Instruction
+  matchRow = do
+    _ <- string "rotate"
+    _ <- space
+    _ <- string "row"
+    _ <- space
+    _ <- string "y="
+    a <- many digit
+    _ <- space
+    _ <- string "by"
+    _ <- space
+    b <- many digit
+    return $ Row (read a) (read b)
+
+  matchColumn :: Parser Instruction
+  matchColumn = do
+    _ <- string "rotate"
+    _ <- space
+    _ <- string "column"
+    _ <- space
+    _ <- string "x="
+    a <- many digit
+    _ <- space
+    _ <- string "by"
+    _ <- space
+    b <- many digit
+    return $ Column (read a) (read b)
 
--- tests/Day8Tests.hs
+++ tests/Day8Tests.hs
@@ -1,6 +1,6 @@
 module Day8Tests where
   import Test.HUnit
-  import Day8 (resulta, resultb, rotate)
+  import Day8 (resulta, resultb, rotate, toInstructions, Instruction(On,Row,Column))
 
   day8 :: String -> Test
   day8 a = test [
@@ -12,5 +12,6 @@ module Day8Tests where
       assertEqual "rotate" [1,0,0] (rotate [0,0,1] 4),
       assertEqual "rotate" [0,1,0] (rotate [0,0,1] 5),
       assertEqual "rotate" [0,0,1] (rotate [0,0,1] 6),
-      assertEqual "rotate" [1,0,0] (rotate [0,0,1] 7)
+      assertEqual "rotate" [1,0,0] (rotate [0,0,1] 7),
+      assertEqual "instructions" [] (toInstructions a)
     ]
