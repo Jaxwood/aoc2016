e3f3ff2f27a992748b2820422abd6c7b5b9e2771
Jacob Lorenzen <lorenzen.jacob@gmail.com>
2017-11-30 13:08:25 +0100

    parse instructions

--- src/Day10.hs
+++ src/Day10.hs
@@ -1,9 +1,43 @@
 module Day10 (resulta, resultb) where
 
   import Data.List
+  import Data.Either (rights)
+  import Text.Parsec
+  import Text.Parsec.String
 
-  resulta :: String -> [Int] -> Int
-  resulta x xs = head xs
+  type Value = Int
+  type Low = Int
+  type High = Int
+  data Reciever = Bot Int | Output Int deriving (Show)
+  data Instruction = Assign Reciever Value | Logic Reciever Low High Reciever deriving (Show)
+
+  resulta :: String -> [Int] -> [Instruction]
+  resulta x xs = parseInstructions $ lines x
 
   resultb :: String -> String
   resultb = id
+
+  parseInstructions :: [String] -> [Instruction]
+  parseInstructions = rights . map (parse (choice [parseRecieverInstruction, parseValueInstruction]) "")
+
+  parseRecieverInstruction :: Parser Instruction
+  parseRecieverInstruction = do
+    _ <- string "bot "
+    bot <- many1 digit
+    _ <- string " gives low to "
+    lowTo <- choice [string "bot", string "output"]
+    _ <- space
+    low <- many1 digit
+    _ <- string " and high to "
+    highTo <- choice [string "bot", string "output"]
+    _ <- space
+    high <- many1 digit
+    return $ Logic (if lowTo == "bot" then Bot (read low) else Output (read low)) (read low) (read high) (if highTo == "bot" then Bot (read high) else Output (read high))
+
+  parseValueInstruction :: Parser Instruction
+  parseValueInstruction = do
+    _ <- string "value "
+    v <- many1 digit
+    _ <- string " goes to bot "
+    b <- many1 digit
+    return $ Assign (Bot $ read b) (read v)
