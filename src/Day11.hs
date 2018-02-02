b6c548371fd2618f05d64bc838e870adb0052128
Jacob Lorenzen <lorenzen.jacob@gmail.com>
2018-02-02 20:35:34 +0100

    Introduce validate
    validate will look at two RTG and verify that they can co-exists

--- src/Day11.hs
+++ src/Day11.hs
@@ -1,7 +1,15 @@
-module Day11 (resulta, resultb) where
+module Day11 (resulta, resultb, RTG(Generator,Microchip), validate) where
+ 
+  data RTG = Generator Char | Microchip Char deriving (Eq,Show)
 
-  resulta :: [[String]] -> Int
+  resulta :: [[RTG]] -> Int
   resulta = length
 
   resultb :: String -> Int
   resultb s = 0
+
+  validate :: RTG -> RTG -> Bool
+  validate (Generator _) (Generator _) = True
+  validate (Microchip x) (Generator y) = x == y
+  validate (Generator x) (Microchip y) = x == y
+  validate (Microchip _) (Microchip _) = True
--- tests/Day11Tests.hs
+++ tests/Day11Tests.hs
@@ -2,10 +2,15 @@ module Day11Tests where
 
   import Test.HUnit
   import qualified Data.Map as M
-  import Day11 (resulta, resultb)
+  import Day11 (resulta, resultb, RTG(Generator,Microchip),validate)
 
   day11 :: String -> Test
   day11 a = test [
-      assertEqual "day11a" 11 (resulta [["LM","HM"],["HG"],["LG"],[]]),
-      assertEqual "day11b" 0 (resultb a)
+      assertEqual "day11a" 11 (resulta [[Microchip 'L', Microchip 'H'],[Generator 'H'],[Generator 'L'],[]]),
+      assertEqual "day11b" 0 (resultb a),
+      assertEqual "validate" True (validate (Microchip 'L') (Microchip 'H')),
+      assertEqual "validate" True (validate (Microchip 'L') (Generator 'L')),
+      assertEqual "validate" True (validate (Microchip 'H') (Generator 'H')),
+      assertEqual "validate" False (validate (Generator 'L') (Microchip 'H')),
+      assertEqual "validate" False (validate (Generator 'H') (Microchip 'L'))
     ]
