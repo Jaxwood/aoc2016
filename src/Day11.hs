1c0067fe1cf2195c9adb6577c6b0f6a53d509b0f
Jacob Lorenzen <jacob@lorenzen.me>
2020-04-26 23:48:14 +0200

    Add testcase for level 2

--- src/Day11.hs
+++ src/Day11.hs
@@ -1,8 +1,9 @@
-module Day11 (resulta, RTG(Generator,Microchip), validate) where
+module Day11 (resulta, RTG(Generator,Microchip), validate, move) where
  
   import qualified Data.Map as M
   import qualified Data.Set as S
   import Data.List
+  import Control.Monad as Mo
   import Data.Maybe
 
   data RTG = Generator Char | Microchip Char deriving (Eq,Show)
@@ -10,8 +11,7 @@ module Day11 (resulta, RTG(Generator,Microchip), validate) where
   instance Ord RTG where
     (Generator x) `compare` (Generator y) = x `compare` y
     (Microchip x) `compare` (Microchip y) = x `compare` y
-    (Microchip x) `compare` (Generator y) = x `compare` y
-    (Generator x) `compare` (Microchip y) = x `compare` y
+    _ `compare` _ = LT
 
   -- only combinations of length (1|2) is valid
   withinRange :: [[RTG]] -> [[RTG]]
@@ -25,13 +25,17 @@ module Day11 (resulta, RTG(Generator,Microchip), validate) where
 
   -- move the elevator
   move :: M.Map Int [RTG] -> Int -> [RTG] -> [M.Map Int [RTG]]
-  move m lvl rs
-    | lvl == 1 = [M.update (const addedAbove) (lvl + 1) (M.update (const removed) lvl m)]
-    | lvl == 4 =[M.update (const addedBelow) (lvl - 1) (M.update (const removed) lvl m)]
-    | otherwise = [
-        M.update (const addedAbove) (lvl + 1) (M.update (const removed) lvl m),
-        M.update (const addedBelow) (lvl - 1) (M.update (const removed) lvl m)
-      ]
+  move m lvl rs =
+    case [validate current, validate addedAbove, validate addedBelow] of
+      [True, True, True] ->
+        [ M.update (const addedAbove) (lvl + 1) (M.update (const removed) lvl m),
+          M.update (const addedBelow) (lvl - 1) (M.update (const removed) lvl m)]
+      [True, True, False] ->
+        [M.update (const addedAbove) (lvl + 1) (M.update (const removed) lvl m)]
+      [True, False, True] ->
+        [M.update (const addedBelow) (lvl - 1) (M.update (const removed) lvl m)]
+      otherwise ->
+        []
     where current = M.lookup lvl m
           above = M.lookup (lvl + 1) m
           below = M.lookup (lvl - 1) m
@@ -52,8 +56,11 @@ module Day11 (resulta, RTG(Generator,Microchip), validate) where
   isValid rtgs _ acc = acc
 
   -- validate the building floor
-  validate :: [RTG] -> Bool
-  validate rtgs = foldr (isValid rtgs) True rtgs
+  validate :: Maybe [RTG] -> Bool
+  validate rtgs =
+    case rtgs of
+      (Just rs) -> foldr (isValid rs) True rs 
+      Nothing -> False
 
   -- check for generator
   isGenerator :: RTG -> Bool
--- tests/Day11Tests.hs
+++ tests/Day11Tests.hs
@@ -3,18 +3,24 @@ module Day11Tests where
   import Test.HUnit
   import Data.Maybe
   import qualified Data.Map as M
-  import Day11 (resulta, RTG(Generator,Microchip), validate)
+  import Day11 (resulta, RTG(Generator,Microchip), validate, move)
 
   day11 :: String -> Test
   day11 a = test [
-      assertEqual "day11a" Nothing (resulta (M.fromList [
+      assertEqual "day11a - step 1" Nothing (resulta (M.fromList [
         (1, [Microchip 'L', Microchip 'H']),
         (2, [Generator 'H']),
         (3, [Generator 'L']),
         (4, [])]) 1),
-      assertEqual "validate" True (validate [(Generator 'L'), (Microchip 'L')]),
-      assertEqual "validate" False (validate [(Generator 'L'), (Microchip 'H')]),
-      assertEqual "validate" True (validate [(Microchip 'L'), (Microchip 'H')]),
-      assertEqual "validate" True (validate [(Generator 'L'), (Generator 'H')]),
-      assertEqual "validate" True (validate [(Generator 'H'), (Generator 'L'), (Microchip 'H')])
+      assertEqual "day11a - step 2" Nothing (resulta (M.fromList [
+        (1,[Microchip 'L']),
+        (2,[Generator 'H', Microchip 'H']),
+        (3,[Generator 'L']),
+        (4,[])]) 2),
+      assertEqual "validate" True (validate $ Just [(Generator 'L'), (Microchip 'L')]),
+      assertEqual "validate" False (validate $ Just [(Generator 'L'), (Microchip 'H')]),
+      assertEqual "validate" True (validate $ Just [(Microchip 'L'), (Microchip 'H')]),
+      assertEqual "validate" True (validate $ Just [(Generator 'L'), (Generator 'H')]),
+      assertEqual "validate" True (validate $ Just [(Generator 'H'), (Generator 'L'), (Microchip 'H')]),
+      assertEqual "validate" True (validate $ Just [])
     ]
