d6a2cdd22e5458b0259eca67b9d4c998acddb764
Jacob Lorenzen <jacob@lorenzen.me>
2020-04-26 17:11:22 +0200

    Introduce combinations function

--- src/Day11.hs
+++ src/Day11.hs
@@ -1,8 +1,26 @@
 module Day11 (resulta, RTG(Generator,Microchip)) where
  
-  import Data.Map as M
+  import qualified Data.Map as M
+  import qualified Data.Set as S
+  import Data.Maybe
 
   data RTG = Generator Char | Microchip Char deriving (Eq,Show)
 
-  resulta :: M.Map Int [RTG] -> Int
-  resulta = length
+  instance Ord RTG where
+    (Generator x) `compare` (Generator y) = x `compare` y
+    (Microchip x) `compare` (Microchip y) = x `compare` y
+    (Microchip x) `compare` (Generator y) = x `compare` y
+    (Generator x) `compare` (Microchip y) = x `compare` y
+
+  -- only combinations of length (1|2) is valid
+  withinRange :: [[RTG]] -> [[RTG]]
+  withinRange = filter ((\x -> (x > 0) && (x < 3)) . length) 
+
+  -- get all the combinations for microchip and generator on a certain building floor
+  combinations :: Int -> M.Map Int [RTG] -> Maybe [[RTG]]
+  combinations lvl m =
+    let combos = map S.toList . S.toList . S.powerSet . S.fromList <$> M.lookup lvl m
+    in withinRange <$> combos
+
+  resulta :: M.Map Int [RTG] -> Maybe [[RTG]]
+  resulta m = combinations 1 m
--- tests/Day11Tests.hs
+++ tests/Day11Tests.hs
@@ -1,12 +1,13 @@
 module Day11Tests where
 
   import Test.HUnit
+  import Data.Maybe
   import qualified Data.Map as M
   import Day11 (resulta, RTG(Generator,Microchip))
 
   day11 :: String -> Test
   day11 a = test [
-      assertEqual "day11a" 11 (resulta $ M.fromList [
+      assertEqual "day11a" Nothing (resulta $ M.fromList [
         (1, [Microchip 'L', Microchip 'H']),
         (2, [Generator 'H']),
         (3, [Generator 'L']),
