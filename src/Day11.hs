fb57b94d98a679ed2573f249e907fbe412d4f553
Jacob Lorenzen <jacob@lorenzen.me>
2020-04-26 15:05:03 +0200

    Add failing testcase

--- src/Day11.hs
+++ src/Day11.hs
@@ -1,18 +1,8 @@
-module Day11 (resulta, resultb, RTG(Generator,Microchip), validate) where
+module Day11 (resulta, RTG(Generator,Microchip)) where
  
+  import Data.Map as M
+
   data RTG = Generator Char | Microchip Char deriving (Eq,Show)
 
-  resulta :: [[RTG]] -> Int
+  resulta :: M.Map Int [RTG] -> Int
   resulta = length
-
-  resultb :: String -> Int
-  resultb s = 0
-
-  validate :: [RTG] -> [RTG] -> Bool
-  validate [] _ = True
-  validate ((Generator _):rs) rts = and $ [True,validate rs rts]
-  validate ((Microchip x):rs) rts = and $ [or [(Generator x) `elem` rts, all (not . isGenerator) rts], validate rs rts]
-
-  isGenerator :: RTG -> Bool
-  isGenerator (Microchip x) = False
-  isGenerator (Generator x) = True
--- tests/Day11Tests.hs
+++ tests/Day11Tests.hs
@@ -2,17 +2,13 @@ module Day11Tests where
 
   import Test.HUnit
   import qualified Data.Map as M
-  import Day11 (resulta, resultb, RTG(Generator,Microchip),validate)
+  import Day11 (resulta, RTG(Generator,Microchip))
 
   day11 :: String -> Test
   day11 a = test [
-      --assertEqual "day11a" 11 (resulta [[Microchip 'L', Microchip 'H'],[Generator 'H'],[Generator 'L'],[]]),
-      assertEqual "day11b" 0 (resultb a),
-      assertEqual "validate" True (validate [] []),
-      assertEqual "validate" True (validate [Generator 'L'] [Generator 'L']),
-      assertEqual "validate" True (validate [Microchip 'L'] [Microchip 'L']),
-      assertEqual "validate" True (validate [Generator 'L', Microchip 'L'] [Generator 'L', Microchip 'L']),
-      assertEqual "validate" False (validate [Generator 'H', Microchip 'L'] [Generator 'H', Microchip 'L']),
-      assertEqual "validate" True (validate [Microchip 'H', Microchip 'L'] [Microchip 'H', Microchip 'L']),
-      assertEqual "validate" True (validate [Generator 'H', Generator 'L'] [Generator 'H', Generator 'L'])
+      assertEqual "day11a" 11 (resulta $ M.fromList [
+        (1, [Microchip 'L', Microchip 'H']),
+        (2, [Generator 'H']),
+        (3, [Generator 'L']),
+        (4, [])])
     ]
