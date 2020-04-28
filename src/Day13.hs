4c36ff65cae88977efbc2c4c3246e7b76e6f4bef
Jacob Lorenzen <jacob@lorenzen.me>
2020-04-28 08:16:13 +0200

    Solution for day13b

--- src/Day13.hs
+++ src/Day13.hs
@@ -1,4 +1,4 @@
-module Day13 (resulta) where
+module Day13 (resulta, resultb) where
 
   import Data.Bits
   import qualified Data.Set as S
@@ -8,18 +8,22 @@ module Day13 (resulta) where
   bitcount num acc
     | num == 0 = acc
     | otherwise = bitcount (num .&. (num - 1)) (acc + 1)
-  
+
+  -- check if the square is open
   open :: Int -> (Int, Int, Int) -> Bool
   open num (x, y, _) = let candidate = (x * x) + (3 * x) + (2*x*y) + y + (y*y) + num
                     in even $ bitcount candidate 0
 
+  -- add two coordinates
   add :: (Int, Int, Int) -> (Int, Int) -> (Int,Int, Int)
   add (x,y,m) (x',y') = (x+x', y+y', (m+1))
 
+  -- find neighbor squares
   neighbors :: (Int, Int, Int) -> [(Int, Int, Int)]
   neighbors coord = filter (\(x,y,_) -> and [(x >= 0), (y >= 0)]) coords
     where coords = map (add coord) [(1,0), ((-1), 0), (0, 1), (0, (-1))]
 
+  -- explore the maze and find the moves till target square
   explore :: (Int, Int) -> Int -> S.Set (Int,Int) -> [(Int, Int, Int)] -> Int 
   explore target fav visited [] = 0
   explore target fav visited ((x,y,m):xs)
@@ -30,4 +34,16 @@ module Day13 (resulta) where
 
   resulta :: (Int, Int) -> Int -> Int
   resulta (x,y) fav = explore (x,y) fav (S.singleton (1,1)) $ filter (open fav) $ neighbors start
+    where start = (1,1,0)
+
+  -- explore the maze and find the number of squares that can be reached within 50 moves
+  explore' :: Int -> S.Set (Int,Int) -> [(Int, Int, Int)] -> Int 
+  explore' fav visited [] = length visited
+  explore' fav visited ((x,y,m):xs)
+    | m > 50 || (S.member (x,y) visited) = explore' fav visited xs
+    | otherwise = explore' fav (S.insert (x,y) visited) (xs ++ next)
+      where next = filter (open fav) $ neighbors (x,y,m)
+
+  resultb :: Int -> Int
+  resultb fav = explore' fav (S.singleton (1,1)) $ filter (open fav) $ neighbors start
     where start = (1,1,0)
--- tests/Day13Tests.hs
+++ tests/Day13Tests.hs
@@ -1,10 +1,11 @@
 module Day13Tests where
 
   import Test.HUnit
-  import Day13 (resulta)
+  import Day13 (resulta, resultb)
 
   day13 :: Test
   day13 = test [
       assertEqual "day13a" 11 (resulta (7,4) 10),
-      assertEqual "day13a" 82 (resulta (31,39) 1362)
+      assertEqual "day13a" 82 (resulta (31,39) 1362),
+      assertEqual "day13b" 138 (resultb 1362)
     ]
