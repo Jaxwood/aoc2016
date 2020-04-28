2c6aee4ef42ae0f0522961c01eb6afd70f5d425f
Jacob Lorenzen <jacob@lorenzen.me>
2020-04-28 08:02:42 +0200

    Solution for day13a

--- src/Day13.hs
+++ src/Day13.hs
@@ -1,15 +1,33 @@
 module Day13 (resulta) where
 
   import Data.Bits
+  import qualified Data.Set as S
 
+  -- Brian Kernighan’s Algorithm
   bitcount :: Int -> Int -> Int
   bitcount num acc
     | num == 0 = acc
     | otherwise = bitcount (num .&. (num - 1)) (acc + 1)
   
-  open :: Int -> (Int, Int) -> Bool
-  open num (x, y) = let candidate = (x * x) + (3 * x) + (2*x*y) + y + (y*y) + num
+  open :: Int -> (Int, Int, Int) -> Bool
+  open num (x, y, _) = let candidate = (x * x) + (3 * x) + (2*x*y) + y + (y*y) + num
                     in even $ bitcount candidate 0
 
-  resulta :: (Int, Int)  -> Int -> Bool
-  resulta (x,y) a = open a (x,y)
+  add :: (Int, Int, Int) -> (Int, Int) -> (Int,Int, Int)
+  add (x,y,m) (x',y') = (x+x', y+y', (m+1))
+
+  neighbors :: (Int, Int, Int) -> [(Int, Int, Int)]
+  neighbors coord = filter (\(x,y,_) -> and [(x >= 0), (y >= 0)]) coords
+    where coords = map (add coord) [(1,0), ((-1), 0), (0, 1), (0, (-1))]
+
+  explore :: (Int, Int) -> Int -> S.Set (Int,Int) -> [(Int, Int, Int)] -> Int 
+  explore target fav visited [] = 0
+  explore target fav visited ((x,y,m):xs)
+    | (x,y) == target = m
+    | S.member (x,y) visited = explore target fav visited xs
+    | otherwise = explore target fav (S.insert (x,y) visited) (xs ++ next)
+      where next = filter (open fav) $ neighbors (x,y,m)
+
+  resulta :: (Int, Int) -> Int -> Int
+  resulta (x,y) fav = explore (x,y) fav (S.singleton (1,1)) $ filter (open fav) $ neighbors start
+    where start = (1,1,0)
--- tests/Day13Tests.hs
+++ tests/Day13Tests.hs
@@ -5,5 +5,6 @@ module Day13Tests where
 
   day13 :: Test
   day13 = test [
-      assertEqual "day13a" True (resulta (7,4) 10) -- 1362
+      assertEqual "day13a" 11 (resulta (7,4) 10),
+      assertEqual "day13a" 82 (resulta (31,39) 1362)
     ]
