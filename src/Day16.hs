35b4dffb9951ac52fa513397e4981f36f6c337d0
Jacob Lorenzen <jacob@lorenzen.me>
2020-04-29 17:22:54 +0200

    Solution for day16b

--- src/Day16.hs
+++ src/Day16.hs
@@ -1,16 +1,19 @@
 module Day16 (resulta) where
 
+  -- inverse the character
   inverse :: Char -> Char
   inverse c
     | c == '0' = '1'
     | c == '1' = '0'
 
+  -- calculate checksum
   checksum :: String -> String -> String
-  checksum acc [] = if even (length acc) then checksum "" acc else acc
+  checksum acc [] = if even (length acc) then checksum "" (reverse acc) else (reverse acc)
   checksum acc (a:b:xs)
     | a == b = checksum ('1':acc) xs
     | otherwise = checksum ('0':acc) xs
 
+  -- generate length amount of data
   resulta :: Int -> String -> String
   resulta len a = if length a' < len then resulta len a' else checksum "" $ take len a'
     where b = map inverse $ reverse a
--- tests/Day16Tests.hs
+++ tests/Day16Tests.hs
@@ -6,5 +6,6 @@ module Day16Tests where
   day16 :: Test
   day16 = test [
       assertEqual "day16a" "01100" (resulta 20 "10000"),
-      assertEqual "day16a" "" (resulta 272 "10001110011110000")
+      assertEqual "day16a" "10010101010011101" (resulta 272 "10001110011110000"),
+      assertEqual "day16b" "01100111101101111" (resulta 35651584 "10001110011110000")
     ]
