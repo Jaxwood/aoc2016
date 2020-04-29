8e807a834fd76519b81997749a530c34ad8c7e42
Jacob Lorenzen <jacob@lorenzen.me>
2020-04-29 14:16:19 +0200

    Add comments

--- src/Day15.hs
+++ src/Day15.hs
@@ -2,12 +2,14 @@ module Day15 (resulta) where
 
   import Data.Monoid
 
+  -- check if the disc is in the open position
   valid :: (Int, Int) -> Int -> All
   valid (pos, idx) time = All $ (idx + time) `mod` pos == 0
 
+  -- move the discs
   generate :: [(Int, Int)] -> Int -> All
   generate discs num = mconcat $ zipWith valid discs [(num + 1)..]
 
+  -- keep moving the disc until everything is aligned
   resulta :: [(Int, Int)] -> Int
-  resulta discs = length lst
-    where lst = takeWhile (not . getAll) $ map (generate discs) [0..]
+  resulta discs = length $ takeWhile (not . getAll) $ map (generate discs) [0..]
--- tests/Day15Tests.hs
+++ tests/Day15Tests.hs
@@ -6,5 +6,5 @@ module Day15Tests where
   day15 :: Test
   day15 = test [
       assertEqual "day15a" 5 (resulta [(5,4),(2,1)]),
-      assertEqual "day15a" 0 (resulta [(17,15),(3,2),(19,4),(13,2),(7,2),(5,0)])
+      assertEqual "day15a" 400589 (resulta [(17,15),(3,2),(19,4),(13,2),(7,2),(5,0)])
     ]
