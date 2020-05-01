c33cfb37b0f7da9f4afc5c1deedeb3a42e4db345
Jacob Lorenzen <jacob@lorenzen.me>
2020-05-01 11:45:25 +0200

    Solution for day19b
    The tricky part is to keep the index updated along the way

--- src/Day19.hs
+++ src/Day19.hs
@@ -1,13 +1,28 @@
-module Day19 (resulta) where
+module Day19 (resulta, resultb) where
 
-  data Inventory = Present Int deriving (Eq,Show)
+  import qualified Data.Sequence as S
+  import Data.Maybe
 
-  game :: [Inventory] -> [Inventory] -> Int
-  game ((Present x):[]) [] = x
+  game :: [Maybe Int] -> [Maybe Int] -> Int
+  game ((Just x):[]) [] = x
   game acc [] = game [] (reverse acc)
-  game acc ((Present x):[]) = game (acc ++ [(Present x)]) []
-  game acc ((Present x):(Present y):ts) = game ((Present x):acc) ts
+  game acc ((Just x):[]) = game (acc ++ [(Just x)]) []
+  game acc ((Just x):(Just y):ts) = game ((Just x):acc) ts
 
   resulta :: Int -> Int
   resulta num = game [] participants
-    where participants = map Present [1..num]
+    where participants = map Just [1..num]
+
+  game' :: Int -> S.Seq (Maybe Int) -> S.Seq (Maybe Int)
+  game' idx participants = if S.length participants == 1 then participants else game' idx' next
+    where len = S.length participants
+          newIdx = ((len `div` 2) + idx) `mod` len
+          (a,b) = S.splitAt newIdx participants
+          next = (S.><) a (S.drop 1 b)
+          loc = if newIdx >= idx then idx else pred idx
+          idx' = if loc == S.length next - 1 then 0 else succ loc
+
+
+  resultb :: Int -> Int -> S.Seq (Maybe Int)
+  resultb idx num = game' idx participants
+    where participants = S.fromList $ map Just [1..num]
--- tests/Day19Tests.hs
+++ tests/Day19Tests.hs
@@ -1,10 +1,15 @@
 module Day19Tests where
 
   import Test.HUnit
-  import Day19 (resulta)
+  import qualified Data.Sequence as S
+  import Day19 (resulta, resultb)
 
   day19 :: Test
   day19 = test [
       assertEqual "day19a" 3 (resulta 5),
-      assertEqual "day19a" 1808357 (resulta 3001330)
+      assertEqual "day19a" 1808357 (resulta 3001330),
+      assertEqual "day19b" (S.singleton (Just 2)) (resultb 0 5),
+      assertEqual "day19b" (S.singleton (Just 1)) (resultb 0 4),
+      assertEqual "day19b" (S.singleton (Just 3)) (resultb 0 6),
+      assertEqual "day19b" (S.singleton (Just 1407007)) (resultb 0 3001330)
     ]
