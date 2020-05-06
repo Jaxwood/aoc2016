df7ccdd9818bce326cee353ee7c30e6d6e67e639
Jacob Lorenzen <jacob@lorenzen.me>
2020-05-06 12:23:38 +0200

    Solution for day23b

--- tests/Day23Tests.hs
+++ tests/Day23Tests.hs
@@ -7,4 +7,9 @@ module Day23Tests where
   day23 :: String -> Test
   day23 a = test [
       assertEqual "day23a" 10953 (resulta a $ M.fromList [("a", 7), ("b", 0), ("c", 0), ("d", 0)])
+      -- Part b:
+      -- running the code will take too long time..
+      -- after analyzing the instructions the equation is (a! + k)
+      -- k is determined by the last 7 instructions e.g. 87*73 = 5913
+      -- the solution will then be = 12! + 5913 = 479007513
     ]
