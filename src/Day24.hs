85a9bfcc037644adb385d67078df1b1683cea0e4
Jacob Lorenzen <jacob@lorenzen.me>
2020-05-07 21:53:13 +0200

    Solution for day24a

--- src/Day24.hs
+++ src/Day24.hs
@@ -1,18 +1,15 @@
 module Day24 (resulta) where
 
+  import Data.List
   import qualified Data.Map as M
   import qualified Data.Set as S
 
   data Location = Wall | Open | Blueprint Char deriving (Eq,Show)
 
   type Grid = M.Map (Int, Int) Location
-
-  data State = State {
-    current :: (Int, Int),
-    moves :: Int,
-    visited :: S.Set (Int, Int),
-    blueprints :: S.Set Char
-  }
+  type Memory = M.Map Char Int
+  type Global = M.Map Char Memory
+  type Coord = ((Int,Int), Int)
 
   location :: Char -> Location
   location c
@@ -26,34 +23,36 @@ module Day24 (resulta) where
   parse :: String -> Grid
   parse s = M.fromList coords
     where lns = lines s
-          coords = concat $ zipWith line lns [0..] 
+          coords = concat $ zipWith line lns [0..]
   
-  neighbors :: State -> Grid -> (Int, Int) -> [(Int, Int)]
-  neighbors s g (x,y) = filter (\a -> S.notMember a visit) $ filter (\c -> (g M.! c) /= Wall) $ map (\(x',y') -> (x+x',y+y')) [(0,1), (1,0), (-1,0), (0,-1)]
-    where visit = visited s
+  neighbors :: Grid -> S.Set (Int, Int) -> (Int, Int) -> [(Int, Int)]
+  neighbors g v (x,y) = filter (\a -> S.notMember a v) $ filter (\c -> (g M.! c) /= Wall) $ map (\(x',y') -> (x+x',y+y')) [(0,1), (1,0), (-1,0), (0,-1)]
+  
+  scan :: Grid -> S.Set (Int, Int) -> [Coord] -> Memory -> Memory
+  scan _ _ [] acc = acc
+  scan g v ((s,m):ss) acc = if S.member s v then scan g v ss acc else scan g v' (ss ++ next') acc'
+    where next = neighbors g v s
+          v' = S.insert s v
+          acc' = case g M.! s of
+            (Blueprint b) -> M.insert b m acc
+            _ -> acc
+          next' = map (\n -> (n, succ m)) next
 
-  toState :: Grid -> State -> (Int, Int) -> State
-  toState g s c = State { current = c, moves = succ m, visited = voc, blueprints = loc }
-    where m = moves s
-          v = visited s
-          b = blueprints s
-          voc = case g M.! c of
-            Open -> S.insert c v
-            (Blueprint v) -> S.singleton c
-          loc = case g M.! c of
-            Open -> b
-            (Blueprint blue) -> S.insert blue b
-
-  travel :: Grid -> [State] -> [Int] -> Int
-  travel _ [] acc = minimum acc
-  travel g (s:ss) acc = if length (blueprints s) == 4 then (moves s) else travel g (ss ++ ss') acc'
-    where next = neighbors s g $ current s
-          ss' = map (toState g s) next
-          acc' = if length (blueprints s) == 4 then (moves s):acc else acc
+  distanceTo :: Global -> Char -> Char -> Int
+  distanceTo g a b = a' M.! b
+    where a' = g M.! a
+  
+  distances :: Global -> [String] -> [Int] -> Int
+  distances _ [] acc = minimum acc
+  distances g (s:ss) acc = distances g ss ((fst res):acc)
+    where res = foldl' (\(a, prev) n -> (a + distanceTo g prev n, n)) (0, head s) (tail s)
+  
+  getLocation :: Location -> Char
+  getLocation (Blueprint c) = c
 
   resulta :: String -> Int
-  resulta a = travel grid' [state] [] -- neighbors state grid' start
-    where grid = parse a
-          grid'= M.insert start Open grid
-          start = fst $ head $ M.toList $ M.filter (== Blueprint '0') grid
-          state = State { current = start, moves = 0, visited = S.empty, blueprints = S.empty }
+  resulta s = distances mem opt []
+    where g = parse s
+          ks = M.toList $ M.filter (\c -> c /= Wall && c /= Open) g
+          opt = filter (\xs -> head xs == '0') $ permutations $ map (getLocation . snd) ks
+          mem = foldr (\n acc -> case (g M.! n) of (Blueprint c) -> M.insert c (scan g S.empty [(n,0)] M.empty) acc) M.empty (map fst ks)
--- tests/Day24Tests.hs
+++ tests/Day24Tests.hs
@@ -1,10 +1,10 @@
 module Day24Tests where
 
   import Test.HUnit
-  import qualified Data.Map as M
   import Day24 (resulta)
 
   day24 :: String -> String -> Test
   day24 a b = test [
-      assertEqual "day24a" 14 (resulta b)
+      assertEqual "day24a" 14 (resulta b),
+      assertEqual "day24a" 428 (resulta a)
     ]
