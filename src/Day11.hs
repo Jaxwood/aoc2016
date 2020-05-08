366f7afe7ce77d260fdb766c4500c50ea55c7f21
Jacob Lorenzen <jacob@lorenzen.me>
2020-05-08 20:17:10 +0200

    Testcase for day11a pass

--- src/Day11.hs
+++ src/Day11.hs
@@ -1,44 +1,25 @@
-module Day11 (resulta, RTG(Elevator,Generator,Microchip), validate, move) where
+module Day11 (resulta, RTG(..), State(..)) where
  
   import qualified Data.Map as M
   import qualified Data.Set as S
   import Data.List
-  import Data.Maybe
-  import Control.Monad
 
-  data RTG = Elevator | Generator Char | Microchip Char deriving (Eq,Show)
+  data RTG = Generator Char | Microchip Char deriving (Eq,Show)
 
   instance Ord RTG where
     (Generator x) `compare` (Generator y) = x `compare` y
     (Microchip x) `compare` (Microchip y) = x `compare` y
     _ `compare` _ = LT
 
-  -- only combinations of length (1|2) is valid
-  withinRange :: [[RTG]] -> [[RTG]]
-  withinRange = filter ((\x -> (x > 0) && (x < 3)) . length) 
+  data State = State {
+    current :: Int,
+    moves :: Int,
+    building :: M.Map Int [RTG]
+  } deriving (Eq, Show)
 
-  -- get all the combinations for microchip and generator on a certain building floor
-  combinations :: M.Map Int [RTG] -> Int -> Maybe [[RTG]]
-  combinations m lvl =
-    let combos = map S.toList . S.toList . S.powerSet . S.fromList <$> M.lookup lvl m
-    in withinRange <$> combos
-
-  -- move the elevator
-  move :: M.Map Int [RTG] -> Int -> [RTG] -> [Maybe (M.Map Int [RTG])]
-  move m lvl rs = [up m, down m]
-    where
-      up = look lvl (\\ rs) >=> look (lvl + 1) (++ rs) >=> look (lvl + 1) (++ [Elevator])
-      down = look lvl (\\ rs) >=> look (lvl - 1) (++ rs) >=> look (lvl - 1) (++ [Elevator])
-  
-  look :: Int -> ([RTG] -> [RTG]) -> M.Map Int [RTG] -> Maybe (M.Map Int [RTG])
-  look k f m = let val = f <$> M.lookup k m
-               in case val of
-                 Nothing -> Nothing
-                 (Just _) -> Just $ M.update (const val) k m
-
-  -- create new maps based on the combinations
-  toMap :: M.Map Int [RTG] -> Int -> [[RTG]] -> [Maybe (M.Map Int [RTG])]
-  toMap m lvl cs = concatMap (move m lvl) cs
+   -- get all the combinations for microchip and generator on a certain building floor
+  combinations :: [RTG] -> [[RTG]]
+  combinations m = filter (\xs -> length xs > 0 && length xs < 3) $ map S.toList $ S.toList $ S.powerSet $ S.fromList m
 
   -- validate the building floor
   isValid :: [RTG] -> RTG -> Bool -> Bool
@@ -49,24 +30,42 @@ module Day11 (resulta, RTG(Elevator,Generator,Microchip), validate, move) where
   isValid rtgs _ acc = acc
 
   -- validate the building floor
-  validate :: Maybe (M.Map Int [RTG]) -> Bool
-  validate rs =
-    case rs of
-      (Just xs) -> M.foldr (\ns a -> foldr (isValid ns) a ns) True xs
-      Nothing -> False
+  validate :: State -> Bool
+  validate s = M.foldr (\ns a -> foldr (isValid ns) a ns) True (building s)
 
   -- check for generator
   isGenerator :: RTG -> Bool
   isGenerator (Generator x) = True
   isGenerator _ = False
 
-  -- check for elevator
-  isElevator :: RTG -> Bool
-  isElevator Elevator = True
-  isElevator _ = False
+  levels :: Int -> [Int]
+  levels l
+    |  l == 1 = [2]
+    |  l == 2 = [1,3]
+    |  l == 3 = [2,4]
+    |  l == 4 = [3]
+  
+  updateBuilding :: State -> (Int, [RTG]) -> State
+  updateBuilding s (l, rts) = s { current = l, moves = succ m, building = final }
+    where lvl = current s
+          b = building s
+          m = moves s
+          next = b M.! l
+          cur = b M.! lvl
+          update = M.insert lvl (sort (cur \\ rts)) b
+          final = M.insert l (sort (next ++ rts)) update
 
-  resulta :: M.Map Int [RTG] -> [Maybe (M.Map Int [RTG])]
-  resulta m = filter validate $ concatMap (toMap withoutElevator lvl) $ combinations withoutElevator lvl 
-              where
-                lvl = fst $ head $ M.toList $ M.filter (any isElevator) m
-                withoutElevator = M.map (filter (not . isElevator)) m
+  move :: State -> [RTG] -> [State]
+  move s rts = map (updateBuilding s) pairs
+    where lvls = levels $ current s
+          pairs = map (\l -> (l, rts)) lvls
+
+  resulta :: [State] -> S.Set (M.Map Int [RTG]) -> Int
+  resulta [] _ = 0
+  resulta (s:ss) v = if done then (moves s) else resulta (ss ++ next) v'
+    where b = building s
+          c = current s
+          v' = S.insert b v
+          combs = combinations $ b M.! c
+          next = filter (\m -> S.notMember (building m) v') $ filter validate $ concatMap (move s) combs
+          done = 4 == length (b M.! 4)
--- tests/Day11Tests.hs
+++ tests/Day11Tests.hs
@@ -1,26 +1,15 @@
 module Day11Tests where
 
   import Test.HUnit
-  import Data.Maybe
   import qualified Data.Map as M
-  import Day11 (resulta, RTG(Elevator,Generator,Microchip), validate, move)
+  import qualified Data.Set as S
+  import Day11 (resulta, RTG(Generator,Microchip), State(..))
 
   day11 :: String -> Test
   day11 a = test [
-      -- assertEqual "day11a - step 1" [] (resulta (M.fromList [
-        -- (1, [Elevator, Microchip 'L', Microchip 'H']),
-        -- (2, [Generator 'H']),
-        -- (3, [Generator 'L']),
-        -- (4, [])])),
-      -- assertEqual "day11a - step 2" [] (resulta (M.fromList [
-        -- (1,[Microchip 'L']),
-        -- (2,[Elevator, Generator 'H', Microchip 'H']),
-        -- (3,[Generator 'L']),
-        -- (4,[])])),
-      assertEqual "validate" True (validate (Just (M.fromList [(1, [(Generator 'L'), (Microchip 'L')])])))
-      -- assertEqual "validate" False (validate [(Generator 'L'), (Microchip 'H')]),
-      -- assertEqual "validate" True (validate [(Microchip 'L'), (Microchip 'H')]),
-      -- assertEqual "validate" True (validate [(Generator 'L'), (Generator 'H')]),
-      -- assertEqual "validate" True (validate [(Generator 'H'), (Generator 'L'), (Microchip 'H')]),
-      -- assertEqual "validate" True (validate [])
+      assertEqual "day11a" 11 (resulta [Day11.State {
+        current = 1,
+        moves = 0,
+        building = M.fromList [(1, [Microchip 'H', Microchip 'L']), (2, [Generator 'H']), (3, [Generator 'L']), (4, [])]
+        }] S.empty)
     ]
